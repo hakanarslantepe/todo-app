6a7262d66f071fe76dd87d8bf058fe0e106eff15
hakanarslantepe <113058120+hakanarslantepe@users.noreply.github.com>
2024-06-11 07:35:18 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["400"] });
 
